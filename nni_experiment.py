604781a2cc094eabe577be4b1891665c6484bfee
Peter Duggins <psipeter@gmail.com>
2022-11-09 15:10:17 -0500

    add py37 empirical data

--- nni_experiment.py
+++ nni_experiment.py
@@ -203,7 +203,7 @@ def main(args):
 
 if __name__ == '__main__':
     params = {
-        'participant_id': 'a1d579a793c1449b944b66bd66e498c9',
+        'participant_id': 'bdee2d88bbb04ff484477ba766b22ad0',
         # 'deltaPs': [0.4, 0.2, 0.1],
         'deltaPs': [0.1],
         'seed': 0,
